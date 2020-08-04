4b2e3a059f5dc630a62a6650c742d6f2dc7cbe31
Jiseon Kim <jsid1561@gmail.com>
2020-08-04 21:38:49 +0900

    Change mid equation in function

--- week4-2.c
+++ week4-2.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
 
 void calcOdd(int arr[], int num);
+void calcEven(int arr[], int num);
 
 int main(void){
     int arr[5] = {1,2,3,4,5};
     int num = sizeof(arr)/sizeof(arr[0]);
 
+    //check number
+    //printf("%d\n", num);
+
     if(num%2)
-        calcOdd();
+        calcOdd(arr,num);
     else
-        calcEven();
+        calcEven(arr,num);
 
     return 0;
 }
 
 void calcOdd(int arr[], int num){
-    int mid = (num+1)/2;
+    int mid = num/2;
     printf("%d\n", arr[mid]);
 }
 
 void calcEven(int arr[], int num){
-    int mid = num/2;
+    int mid = (num-1)/2;
     printf("%d\n", arr[mid]);
 }
 
