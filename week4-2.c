8dc686ac3aa587d957d782d0b842c643ae3ae68d
Jiseon Kim <jsid1561@gmail.com>
2020-08-04 21:34:09 +0900

    Make calcOdd, calcEven function

--- week4-2.c
+++ week4-2.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+void calcOdd(int arr[], int num);
+
 int main(void){
     int arr[5] = {1,2,3,4,5};
     int num = sizeof(arr)/sizeof(arr[0]);
@@ -10,5 +12,15 @@ int main(void){
         calcEven();
 
     return 0;
+}
+
+void calcOdd(int arr[], int num){
+    int mid = (num+1)/2;
+    printf("%d\n", arr[mid]);
+}
+
+void calcEven(int arr[], int num){
+    int mid = num/2;
+    printf("%d\n", arr[mid]);
 }
 
